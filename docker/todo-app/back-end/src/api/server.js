0513086b350c6496f9f3aeaa86b06b97331c7355
unamednada <dias.gustavo.dias@gmail.com>
2022-07-12 11:02:07 -0300

    Revert changes

--- docker/todo-app/back-end/src/api/server.js
+++ docker/todo-app/back-end/src/api/server.js
@@ -2,5 +2,4 @@ const server = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
-server.use(express.static("public"));
 server.listen(PORT, () => console.log("'Trybe Todo List Api' running on port", PORT));
